230e231289df335e21a928ad64775f0173295d3e
Max <tyumenbaev.m@gmail.com>
2021-03-15 22:38:29 +0300

    feat: add new solution

--- src/index.js
+++ src/index.js
@@ -1,56 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-    let chars = str.split(""),
-        array = [],
-        open = [],
-        close = [],
-        closeIndex,
-        openIndex,
-        lenStr = chars.length,
-        matchCheck = false;
-
-
+    const newArr = [];
+    const strArr = str.split(``)
+    const arrBrackets = Object.fromEntries(bracketsConfig);
     
-    for (let i = 0, l = bracketsConfig.length; i < l; i++) {  
-        let charOpen = (bracketsConfig[i])[0],    
-            charClose = (bracketsConfig[i])[1];
-        open.push(charOpen);
-        close.push(charClose);
-    };
-
-    for (let i = 0; i < lenStr; i++) {  
-
-        openIndex = open.indexOf(chars[i]);
-        closeIndex = close.indexOf(chars[i]);
-
-        if (openIndex !== -1 && closeIndex !== -1) {
-            if (matchCheck == false) {
-                array.push(openIndex)
-                matchCheck = true
-            continue;
-            } else {
-                array.pop(closeIndex)
-                matchCheck = false
-                continue;
-            };
+    for (const elem of strArr) {
+        if (newArr[0] === elem) {
+            newArr.shift();
+        } else if (arrBrackets[elem]) {
+            newArr.unshift(arrBrackets[elem]);
         } else {
-
-            if (openIndex !== -1) {
-                array.push(openIndex)
-                continue;
-                };
-        
-            if (closeIndex !== -1) {
-                openIndex = array.pop();
-                if (closeIndex !== openIndex) {
-                    return false;
-                }
-            };
+            return false;
         }
     }
 
-    if (array.length !== 0) {
-        return false;
-    }
-    
-    return true;
+    return newArr.length === 0;
 }
